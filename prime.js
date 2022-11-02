c2a82f8a373bfd00075221258c7b5350690d5d50
sudhanshu4889 <sudhanshu4889@gmail.com>
2022-11-03 00:51:28 +0530

    first commit

---
+++ prime.js
@@ -0,0 +1,15 @@
+
+function checkPrime(num){
+let count=0;
+for(let i=1;i<num;i++){
+	if(num%i==0){
+	    count++;
+	}
+}if(count==2){
+	return true;
+}else{
+	return false;
+}
+}
+let num==13;
+console.loglog("prime no.")
